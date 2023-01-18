5bc27730aa096a78ac01c73393b76f0036b21dea
ayoub3bidi <mrayoubabidi@gmail.com>
2023-01-18 17:29:29 +0100

    feat: add handle events & commands calls (#1)

--- src/bot.js
+++ src/bot.js
@@ -1,18 +1,23 @@
-require("dontenv").config();
+import * as dotenv from 'dotenv'
+dotenv.config()
 const { token } = process.env;
-const { Client, Collection, GatewayIntentBits } = require("discord.js");
-const fs = require("fs");
+import { Client, Collection, GatewayIntentBits } from "discord.js";
+import fs from "fs";
 
 const client = new Client({ intents: GatewayIntentBits.Guilds });
 client.commands = new Collection();
 client.commandsArray = [];
 
-const functionFolders = fs.readdirSync("./functions");
+const functionFolders = fs.readdirSync("./src/functions");
 for (const folder of functionFolders) {
   const functionFiles = fs
-    .readdirSync(`./functions/${folder}`)
+    .readdirSync(`./src/functions/${folder}`)
     .filter((file) => file.endsWith(".js"));
   for (const file of functionFiles) {
-    require(`./functions/${folder}/${file}`)(client);
+    require(`./src/functions/${folder}/${file}`)(client);
   }
 }
+
+client.handleEvents();
+client.handleCommands();
+client.login(token);
