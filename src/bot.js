8958ff42be93b7058a5bf7192e449ff4343d114d
ayoub3bidi <mrayoubabidi@gmail.com>
2023-01-18 16:47:45 +0100

    feat: add handle commands

---
+++ src/bot.js
@@ -0,0 +1,18 @@
+require("dontenv").config();
+const { token } = process.env;
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const fs = require("fs");
+
+const client = new Client({ intents: GatewayIntentBits.Guilds });
+client.commands = new Collection();
+client.commandsArray = [];
+
+const functionFolders = fs.readdirSync("./functions");
+for (const folder of functionFolders) {
+  const functionFiles = fs
+    .readdirSync(`./functions/${folder}`)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of functionFiles) {
+    require(`./functions/${folder}/${file}`)(client);
+  }
+}
